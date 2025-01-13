50442ae017540f1e01deca20e26ac47cb3d4bb81
SaddamHilles <sdm.hilles@gmail.com>
2025-01-13 06:57:52 +0200

    pending status in button when submitting form

--- src/components/UpdateProfileForm/index.tsx
+++ src/components/UpdateProfileForm/index.tsx
@@ -3,6 +3,7 @@
 import { updateGuest } from '@/lib/actions';
 import { Guest } from '@/services/data-services';
 import React, { ReactNode, useState } from 'react';
+import { useFormStatus } from 'react-dom';
 
 interface Props {
   children: ReactNode;
@@ -64,12 +65,21 @@ function UpdateProfileForm({ guest, children }: Props) {
       </div>
 
       <div className='flex justify-end items-center gap-6'>
-        <button className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
   );
 }
 
+function Button() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
+      disabled={pending}
+    >
+      {pending ? 'Updating...' : 'Update profile'}
+    </button>
+  );
+}
 export default UpdateProfileForm;
